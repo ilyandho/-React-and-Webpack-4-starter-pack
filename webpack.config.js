e608fe98bb347e3558bfac89805224a594c3cfc0
ilyandho <ii.ndhote@gmail.com>
2019-07-17 11:18:24 +0300

    Adding post-css

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,7 @@
 const path = require("path");
 const HtmlWepackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-//const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-/*
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "index_bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader
-          },
-          "handlebars-loader", // handlebars loader expects raw resource string
-          "extract-loader",
-          "css-loader"
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWepackPlugin({
-      template: "./src/index.html"
-    }),
-    new MiniCssExtractPlugin({})
-  ]
-};
 
-*/
-
-//const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
